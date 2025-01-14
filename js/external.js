9105eb631d1a52e7523532a2e02246df03fb16bf
roger-anish <rfederer75@gmail.com>
2025-01-14 18:00:11 +0545

    WMS layer and filter
    added a wms layer and applied a cql filter based on selection of attribute

--- js/external.js
+++ js/external.js
@@ -5,8 +5,8 @@ const btn1_dom= document.querySelector('#btn1'); //css based query
 
 // initialize the map on the "map" div with a given center and zoom
 var map = L.map('map', {
-    center: [28.25, 85.45],
-    zoom: 7
+    center: [40.71, -74],
+    zoom: 15
 });
 
 const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -14,32 +14,52 @@ const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var nexrad = L.tileLayer.wms("http://localhost:8080/geoserver/nurc/wms", {
-    layers: 'nexrad-n0r-900913',
+var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
+
+var world_map = L.tileLayer.wms("http://localhost:8080/geoserver/ne/wms?service=WMS", {
+    layers: 'ne:world',
     format: 'image/png',
     transparent: true,
-    attribution: "Weather data © 2012 IEM Nexrad"
-    // cql_filter: " "
+    attribution: "Political map of the world"
 });
+
+var Manhattan = L.tileLayer.wms("http://localhost:8080/geoserver/tiger/wms?service=WMS", {
+    layers: 'tiger:poi',
+    format: 'image/png',
+    transparent: true,
+    attribution: "Manhattan (NY) points of interest",
+    // cql_filter: "NAME='lox'"
+}).addTo(map);
+
+// for cql filter in wms layer
+const showloc=function(){
+    const selctedval = document.getElementById("name").value;  // to extract the name of the attribute
+    // console.log(selctedval);  // eg: lox
+    const cqlfil =`NAME = '${selctedval}'`;  // for creating cql filter value
+    // console.log(cqlfil); // eg: NAME='lox'
+    Manhattan.setParams({cql_filter:cqlfil});
+};
 
 const mylocoptions={
 
 }
-
-const mylocation=L.marker([28.25, 85.45]).addTo(map);
+// marker with popup info on click
+const mylocation=L.marker([28.25, 85.45]).bindPopup('Default location');
 
 console.log(mylocation.getLatLng());
 
 L.control.scale({maxWidth:100}).addTo(map);
-
+// lager toggle section
 var baseLayers = {
     "OSM": tiles,
-    "Nexrad": nexrad
+    "Imagery":Esri_WorldImagery,
+    "World Map":world_map,
 };
-
 var overlays = {
-    "Marker": mylocation,
+    "Default Location": mylocation,
+    "Manhattan": Manhattan
     // "Roads": roadsLayer
 };
-
 L.control.layers(baseLayers, overlays).addTo(map);
